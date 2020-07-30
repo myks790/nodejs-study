d44244485f10c7d7fbcda95353ce81be89e170b5
SangHun Kang <myks790@gmail.com>
2020-07-31 00:15:36 +0900

    pug 예제 추가

---
+++ app/뷰 템플릿 pug/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+
+http.createServer(app).listen(3000);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/login', (req, res) => {
+  const context = { userid: req.body.id, username: req.body.name };
+  res.render('login_success', context);
+});
